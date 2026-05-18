2fea79b7a124edd164816792df9886cf2d7a3555
Marius Uhl <marius.uhl@studmail.htw-aalen.de>
2026-05-18 15:25:46 +0200

    Kommentar hinzugefügt

--- src/main/java/hs/aalen/EntwicklungVonInfoSystemenProjektApplication.java
+++ src/main/java/hs/aalen/EntwicklungVonInfoSystemenProjektApplication.java
@@ -3,7 +3,7 @@ package hs.aalen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication    //Hallo Timo, du bist geil Passi imposter
 public class EntwicklungVonInfoSystemenProjektApplication {
 
 	public static void main(String[] args) {
