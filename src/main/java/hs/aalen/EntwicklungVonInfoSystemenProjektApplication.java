0d7c9db444958e66405a52596e89aac8ff951122
Timo Appt <3009368@studmail.htw-aalen.de>
2026-05-18 14:55:13 +0200

    Initial commit: Spring Boot project setup

---
+++ src/main/java/hs/aalen/EntwicklungVonInfoSystemenProjektApplication.java
@@ -0,0 +1,13 @@
+package hs.aalen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntwicklungVonInfoSystemenProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntwicklungVonInfoSystemenProjektApplication.class, args);
+	}
+
+}
