f734431eb01884fdd8ab54025c9aa61074e04bf4
Timo Appt <3009368@studmail.htw-aalen.de>
2026-06-01 14:31:00 +0200

    Setup und application.properties hinzugefügt

--- src/main/java/hs/aalen/EntwicklungVonInfoSystemenProjektApplication.java
+++ src/main/java/hs/aalen/EntwicklungVonInfoSystemenProjektApplication.java
@@ -3,11 +3,8 @@ package hs.aalen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication    //Hallo Timo, du bist geil Passi imposter
-// Hallo Marius. Du hast total recht. Passi voll der Imposter
-// hallo leute 
-// ich bin der beste Männer
-//Test timo
+// Startet die Anwendung
+@SpringBootApplication
 public class EntwicklungVonInfoSystemenProjektApplication {
 
 	public static void main(String[] args) {
