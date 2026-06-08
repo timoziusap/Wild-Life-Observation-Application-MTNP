3721f217af0a9176ee280c987e24ee71f78e6f68
Pascal Witt <witt.pascal@outlook.de>
2026-06-08 11:53:48 +0200

    "test Comment"

--- src/main/java/hs/aalen/EntwicklungVonInfoSystemenProjektApplication.java
+++ src/main/java/hs/aalen/EntwicklungVonInfoSystemenProjektApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // Startet die Anwendung
+//Test Push von Passi
 @SpringBootApplication
 public class EntwicklungVonInfoSystemenProjektApplication {
 
