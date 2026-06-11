4b815efa025d61e9de0e9c0a341890bcb4e771df
Timo Appt <3009368@studmail.htw-aalen.de>
2026-06-11 14:28:26 +0200

    Test commit

--- src/main/java/hs/aalen/EntwicklungVonInfoSystemenProjektApplication.java
+++ src/main/java/hs/aalen/EntwicklungVonInfoSystemenProjektApplication.java
@@ -3,7 +3,7 @@ package hs.aalen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Startet die Anwendung
+// Startet die Anwendung TEST
 @SpringBootApplication
 public class EntwicklungVonInfoSystemenProjektApplication {
 
